4617b8e55bcf339d688e158d6e35b9988f9166b4
Rohit Kumar <152129671+itzerohit@users.noreply.github.com>
2025-07-20 15:40:42 +0530

    Add files via upload

---
+++ Assigment_Operators.py
@@ -0,0 +1,22 @@
+
+
+## Assignement Operator
+'''
+
+1. = , X = 5
+2. += ,  x +=3, x = x + 3
+3. -= ,  x -= 3, x = x-3
+
+'''
+
+X = 5
+print(X)
+
+X = X+5
+print(X)
+
+X+=5
+print(X)
+
+X-=5
+print(X)
