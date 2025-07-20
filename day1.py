e843e1d61e8e407c88df93c8ae3ce13f4a2096f9
Rohit Kumar <152129671+itzerohit@users.noreply.github.com>
2025-07-20 15:40:07 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,60 @@
+'''* What is Python?
+Ans -- Python is an interpreted, object=oriented, high level-programming language programming language with dynamic semantics. 
+
+Interpreted basically menas run the code line by line 
+Complier basically means run all the code ata time try to read at once 
+object oriented basically based on this class are made 
+hig level prograam are those prograam which we can understand like a = 10 numeric value special alphabet 
+dynamic type -- a = 10 when program run then they will knwo the data types 
+
+python syntax are compressed. 
+
+python was introduced by Guido Van Rossum in 1989 
+
+
+
+
+# Features of python 
+1. Easy to Learn and Implement 
+2. Open Source 
+3. Broad Standard Library 
+4. Cross Platform 
+5. work on Interpreter logic 
+6. Multi - paradigm language 
+7. High level Language 
+8. High level Programming 
+9. Extendable Language 
+10.Expressive Programming Language 
+11. GUI Programming 
+
+
+
+# Application of Python 
+
+1. Website and Application Development. 
+2. Data Visualization 
+3. Scientific Calculation 
+4. Machine Learning and Artifiial intelligence 
+
+
+# COmpany Netflix 
+# Faebook
+# Youtube
+# Torent 
+# Instagram. 
+# Quora
+
+'''
+
+
+
+
+
+print("Hello World", "Welcome to Wscubetech", 80)
+
+print(10>5)
+
+
+
+
+
