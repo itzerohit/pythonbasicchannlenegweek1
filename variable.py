ae0d8249261c0bfe791ac4496bb341ab346b3f90
Rohit Kumar <152129671+itzerohit@users.noreply.github.com>
2025-07-20 15:42:09 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,22 @@
+
+## Variable in Python
+
+#1.Never start the name of the variable with number
+#2.Neve give spcae in the variable with name
+
+a = 10
+b = 20
+print(a,b)
+print('a',b)
+print(id(a),id(b)) # to check the id of the varibale using id function
+
+
+c = "hello"
+print(c)
+
+#1a = 20 THis is wrong
+
+a_b=35
+print(a_b)
+
+# _ (underscore can be used at the first while making the variable )
