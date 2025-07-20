5508dde3078a3cbaac59ea7eb38eaab965a995e2
Rohit Kumar <152129671+itzerohit@users.noreply.github.com>
2025-07-20 15:41:53 +0530

    Add files via upload

---
+++ string_concatenation.py
@@ -0,0 +1,10 @@
+a="Hello"
+b="Wscubetech"
+
+print(a+" "+b)
+
+c=20
+print(c+30)
+
+#  print(a+c) -- This will print the error becuase while in concatentaion both side should be in of same operator
+
