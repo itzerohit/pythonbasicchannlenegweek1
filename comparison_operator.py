16efbaf616ae81129fb0e2c1a5323cdd59bee125
Rohit Kumar <152129671+itzerohit@users.noreply.github.com>
2025-07-20 15:41:00 +0530

    Add files via upload

---
+++ comparison_operator.py
@@ -0,0 +1,19 @@
+# Comparison Operators in Python
+'''
+1. == Equal , X == Y
+2. != Not Equal, X! = Y
+3. > Greater than X > Y
+4. < Less than X < Y
+5. >= Greater than or equal to X >= Y
+6. <= Less than or equal to X <= Y
+'''
+
+x=10
+y=20
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
