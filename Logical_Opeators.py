b9b5ea21694b5f9149c0e29185c736a3ed029ea1
Rohit Kumar <152129671+itzerohit@users.noreply.github.com>
2025-07-20 15:41:18 +0530

    Add files via upload

---
+++ Logical_Opeators.py
@@ -0,0 +1,16 @@
+# ek se jada comparison karna ho th
+'''
+1. and -- Return true if both the statements are true
+2. or -- Return True if one of the statements is true
+3. not -- Reverse the result, return False if the result is True
+
+'''
+
+x=10
+y=20
+print(x==10 or x<y or x == y)
+
+
+print(x!=10)
+# But if we use not it will reverse the solution
+print(not x!=10)
