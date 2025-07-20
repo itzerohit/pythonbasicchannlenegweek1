c2df0e2b3a735dfefd08b0c9518c9a8f45b8df7e
Rohit Kumar <152129671+itzerohit@users.noreply.github.com>
2025-07-20 15:40:24 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,25 @@
+   # Operator in Python
+
+# 1. Arithmetic operators
+
+''' * Addition -- X+Y
+    * Subtraction -- X-Y
+    * Multiplication -- X*Y
+    * Division -- X/Y
+    * Modulus -- X % Y
+    * Exponents -- X**Y
+    * Floor division -- X//Y
+
+'''
+
+a = 10
+b = 20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) # Moduls give remaninder value
+print(a**b)#10*20 times 
+print(a//b) #give answer in int 
+
+
